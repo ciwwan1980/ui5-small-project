a9b20fffb8dbc00860aa232d02792a6c45f00827
Ajil Abdalqader <ajilqado@gmail.com>
2021-11-07 00:31:42 +0100

    adding ComponentContainer and manifest.json library

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,16 +1,19 @@
-
-sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-    ], 
-    function (Controller,MessageToast) {
-    "use strict";
+sap.ui.define(["sap/ui/core/mvc/Controller", "sap/m/MessageToast"], function (
+  Controller,
+  MessageToast
+) {
+  "use strict";
 
   return Controller.extend("opensap.myapp.controller.App", {
     onShowHello: function () {
       // show a native JavaScript alert
-     MessageToast.show("hello message")
+      MessageToast.show("hello message");
     },
-    
+    onInit: function () {
+      console.log("it happen before the ui rendering");
+    },
+    onBeforeRendering: function () {
+      console.log("it happen every time ui is rendered");
+    },
   });
 });
