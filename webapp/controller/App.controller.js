0c06b11810f8ab58e004ea3580e08dbd0472894a
Ajil Abdalqader <ajilqado@gmail.com>
2021-11-06 23:17:28 +0100

    Structuring with Controllers and Modules- adding onShowHelo with Press button

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,16 @@
+
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+    ], 
+    function (Controller,MessageToast) {
+    "use strict";
+
+  return Controller.extend("opensap.myapp.controller.App", {
+    onShowHello: function () {
+      // show a native JavaScript alert
+     MessageToast.show("hello message")
+    },
+    
+  });
+});
