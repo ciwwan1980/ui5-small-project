9f6d44299360c29e782ae9f7879a3a12fa9da921
Ajil Abdalqader <ajilqado@gmail.com>
2021-11-07 13:23:56 +0100

    data binding: connecting with Model(data locally storag as json.file and DYNAMICALLY SHOW THE MESSAGE on click instead of hard coding

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -6,8 +6,13 @@ sap.ui.define(["sap/ui/core/mvc/Controller", "sap/m/MessageToast"], function (
 
   return Controller.extend("opensap.myapp.controller.App", {
     onShowHello: function () {
-      // show a native JavaScript alert
-      MessageToast.show("hello message");
+    // read msg from i18n model
+var oBundle = this.getView().getModel("i18n").getResourceBundle();
+var sRecipient =
+this.getView().getModel("helloPanel").getProperty("/recipient/name");
+var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+// show message
+MessageToast.show(sMsg);
     },
     onInit: function () {
       console.log("it happen before the ui rendering");
