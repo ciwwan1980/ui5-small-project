caf961d34f284921e2d7ddcd62decbd2fe19a7c7
Ajil Abdalqader <ajilqado@gmail.com>
2021-11-10 00:10:37 +0100

    adding functionality in app.controller

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,29 +1,42 @@
-
-sap.ui.define(["sap/ui/core/mvc/Controller", "sap/m/MessageToast",
-"opensap/myapp/model/formatter"], function (
-  Controller,
-  MessageToast, 
-  formatter
-) {
-  "use strict";
-
-  return Controller.extend("opensap.myapp.controller.App", {
-    formatter : formatter,
-    onShowHello: function () {
-      // read msg from i18n model
-      var oBundle = this.getView().getModel("i18n").getResourceBundle();
-      var sRecipient = this.getView()
-        .getModel("helloPanel")
-        .getProperty("/recipient/name");
-      var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-      // show message
-      MessageToast.show(sMsg);
-    },
-    onInit: function () {
-      console.log("it happen before the ui rendering");
-    },
-    onBeforeRendering: function () {
-      console.log("it happen every time ui is rendered");
-    },
-  });
-});
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "opensap/myapp/model/formatter",
+  ],
+  function (Controller, MessageToast, Filter, FilterOperator, formatter) {
+    "use strict";
+    return Controller.extend("opensap.myapp.controller.App", {
+      formatter: formatter,
+      onShowHello: function () {
+        // read msg from i18n model
+        var oBundle = this.getView().getModel("i18n").getResourceBundle();
+        var sRecipient = this.getView()
+          .getModel("helloPanel")
+          .getProperty("/recipient/name");
+        var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+        // show message
+        MessageToast.show(sMsg);
+      },
+      onFilterProducts: function (oEvent) {
+        // build filter array
+        var aFilter = [],
+          sQuery = oEvent.getParameter("query"),
+          // retrieve list control
+          oList = this.getView().byId("productsList"),
+          // get binding for aggregation 'items'
+          oBinding = oList.getBinding("items");
+        if (sQuery) {
+          aFilter.push(
+            new Filter("ProductID", FilterOperator.Contains, sQuery)
+          );
+        }
+        // apply filter. an empty filter array simply removes the filter
+        // which will make all entries visible again
+        oBinding.filter(aFilter);
+      },
+    });
+  }
+);
